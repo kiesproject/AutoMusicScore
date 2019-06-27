9ca2e3877aa0e81a945dcd47ad558869d387afd4
Kento <b1701618@planet.kanazawa-it.ac.jp>
2019-06-27 19:47:05 +0900

    Add:ESP_servo_timer

--- ESP_servo_timer.ino
+++ ESP_servo_timer.ino
@@ -1,17 +1,10 @@
-#include "servoLDX_227.hpp"
-#include "servomotor.hpp"
-#include "util.h"
-
-ServoMotor sm(50,0.75,0.000003,270,0,23);
-
-//タイマー割り込みの準備
 volatile int interruptCounter1;
 volatile int interruptCounter2;
 hw_timer_t * timer1 = NULL;
 hw_timer_t * timer2 = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
-//タイマーに必要な関数
+
 void IRAM_ATTR onTimer1(){
   portENTER_CRITICAL_ISR(&timerMux);
   interruptCounter1++;
@@ -33,11 +26,19 @@ void setup() {
   timerAttachInterrupt(timer1, &onTimer1,true);
   timerAttachInterrupt(timer2, &onTimer2,true);
   //マイクロ秒を百万間隔で呼び出す。(1秒ごと)
-  timerAlarmWrite(timer1,100000,true);
-  timerAlarmWrite(timer2,1000000,true);
+  timerAlarmWrite(timer1,1000000,true); //1秒に1回
+  timerAlarmWrite(timer2,10000000,true); //10秒に1回
   timerAlarmEnable(timer1);
   timerAlarmEnable(timer2);
-  sm.setup();
+
+  //servo1
+  ledcSetup(0,50,10);
+  ledcAttachPin(23,0);
+  ledcWrite(0,base_limit(45,0,105));
+  //servo2
+  ledcSetup(1,50,10);
+  ledcAttachPin(22,1);
+  ledcWrite(1,base_limit(50,0,100));
 }
 
 void loop() {
@@ -47,23 +48,40 @@ void loop() {
       interruptCounter1--;
       portEXIT_CRITICAL(&timerMux);
       /* ここから好きな関数を書き、好きな秒数で呼び出せる。 */
-      cycle();
+      servo();
     }
   if (interruptCounter2 > 0) {
       portENTER_CRITICAL(&timerMux);
       interruptCounter2--;
       portEXIT_CRITICAL(&timerMux);
       /* ここから好きな関数を書き、好きな秒数で呼び出せる。 */
-      giveDig_cycle();
+      control();
     }
 }
 
-void cycle(){
-  sm.cycle();
-  Serial.println("cycle");
+void servo(){
+  //ledcWrite(使いたいモーター,duty)
+  //base_limit(最低値,原点0から+-で整数を与える,最高値)
+ ledcWrite(0,base_limit(45,0,105));
+ ledcWrite(1,base_limit(50,0,100));
 }
 
-void giveDig_cycle(){
-  sm.giveDeg(90.0);
-  Serial.println("giveDeg");
+void control(){
+
+}
+
+//baseから+,-で調整する
+int base_limit(int a,int duty,int c){
+  return intlimit(a,75-duty,c);
+}
+
+//限界値で止める
+int intlimit(int a,int duty,int c){
+  if(duty > c){
+    return c;
+  }
+  if(duty < a){
+    return a;
+  }
+  return duty;
 }
